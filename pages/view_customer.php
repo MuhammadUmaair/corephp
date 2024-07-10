51dffaff7c7f83643c68a0a9fefb47b3d1aee845
Muhammad Umair <104490047+MuhammadUmaair@users.noreply.github.com>
2024-07-10 13:01:25 +0500

    Update view_customer.php

--- pages/view_customer.php
+++ pages/view_customer.php
@@ -6,13 +6,19 @@ include('../pages/auth/auth.php');
 checkAuth();
 checkRole('admin');
 include('../templates/header.php');
+$search_value = isset($_GET['search']) ? $_GET['search'] : "";
 ?>
-
 <body>
 
     <?php include('../includes/navbar.php'); ?>
     <div class="container mt-5">
         <h2 class="text-center">View Customers</h2>
+        <form>
+            <div style="float:right;display: flex;" class="mb-2">
+                <input type="text" name="search" placeholder="Search..." value="<?php echo $search_value; ?>" class="form-control mr-1">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+        </form>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -36,15 +42,18 @@ include('../templates/header.php');
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
                 }
-
-                $result = $conn->query("SELECT * FROM customers");
+                $search_query = "SELECT * FROM customers";
+                if(!empty($search_value)){
+                    $search_query .= " Where name Like '%".$search_value."%' OR email Like '%".$search_value."%' OR phone Like '%".$search_value."%' OR address Like '%".$search_value."%'";
+                }
+                $result = $conn->query($search_query);
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
                         echo "<tr>";
                         echo "<td>" . htmlspecialchars($row['id']) . "</td>";
                         echo "<td>" . htmlspecialchars($row['name']) . "</td>";
                         echo "<td>" . htmlspecialchars($row['email']) . "</td>";
-                        echo "<td>" . htmlspecialchars($row['phone_number']) . "</td>";
+                        echo "<td>" . htmlspecialchars($row['phone']) . "</td>";
                         echo "<td>" . htmlspecialchars($row['address']) . "</td>";
                         echo "<td>
                                 <a href='edit_customer.php?id=" . htmlspecialchars($row['id']) . "' class='btn btn-warning btn-sm'>Edit</a>
