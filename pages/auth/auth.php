e246583066287d1048e3c7ae863c098039ad6c02
Muhammad Umair <104490047+MuhammadUmaair@users.noreply.github.com>
2024-07-10 12:59:27 +0500

    Update auth.php

--- pages/auth/auth.php
+++ pages/auth/auth.php
@@ -1,9 +1,15 @@
 <?php
-session_start();
 function checkAuth()
 {
     if (!isset($_SESSION['user_id'])) {
         header("Location: pages/auth/login.php");
+        exit();
+    }
+}
+function checkLogin()
+{
+    if (isset($_SESSION['user_id'])) {
+        header("Location: /");
         exit();
     }
 }
